e2914b709c6f62b36d9203ebf5f18cb47d51da99
yuto ichinose <yuto.0526.sano@gmail.com>
2022-04-18 23:59:09 +0900

    関数コンポーネント書き方忘れた。

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 let count = 0
-const countUp = () => {
+// const [count,setCount] = React.useState(0)
+const up = () => {
   count++
   alert(count)
 }
+// const CountUp = (props) => {
+//   // count++
+//   // alert(count)
+//   return (<>
+//     <Text>{count}</Text>
+//     <Button
+//         title='count'
+//         onPress={(e)=>{setCount((count)=>count + 1)}}
+//         color='#f00'
+//       />
+//   </>)
+// }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text>Hello World {count}</Text>
       <StatusBar style="auto" />
       <Button
         title='count'
-        onPress={countUp}
+        onPress={up}
         color='#f00'
       />
+      {/* <CountUp/> */}
     </View>
   );
 }
